18321322a729fd3196df1aa5fd13dda1ab300f0c
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2026-03-27 09:21:19 -0300

    Implementa calculo de custo e duracao para AtendimentoInternacao e adiciona relatorio detalhado

--- GestaoHospitalar/Entities/AtendimentoInternacao.cs
+++ GestaoHospitalar/Entities/AtendimentoInternacao.cs
@@ -1,18 +1,39 @@
-﻿using GestaoHospitalar.Enums;
+﻿using System.Globalization;
+using System.Text;
+using GestaoHospitalar.Enums;
+using GestaoHospitalar.Services;
 
 namespace GestaoHospitalar.Entities {
-    internal class AtendimentoInternacao : Atendimento {
+    internal class AtendimentoInternacao : Atendimento, IInformacao {
         public int Diarias { get; private set; }
         public AtendimentoInternacao(string paciente, string cpf, PrioridadeAtendimento prioridade, int diarias) : base(paciente, cpf, TipoAtendimento.Internacao, prioridade) {
 
+            if (diarias < 1 || diarias > 30) {
+
+                throw new ArgumentException("Quantidade inválida!");
+            }
+
             Diarias = diarias;
         }
-        protected override decimal CustoBase() => Diarias;
-        public override decimal CustoTotal() {
-            throw new NotImplementedException();
-        }
-        public override TimeSpan Duracao() {
-            throw new NotImplementedException();
+        protected override decimal CustoBase() => 800m * Diarias;
+        private decimal TaxaMonitoramento() => 200m * Diarias;
+        protected override decimal TaxaAdicional() => Prioridade == PrioridadeAtendimento.Urgente ? CustoBase() * 0.30m : 0m;
+        public override TimeSpan Duracao() => Diarias * TimeSpan.FromHours(24);
+        public override decimal CustoTotal() => CustoBase() + TaxaAdicional() + TaxaMonitoramento();
+        public string LerInformacaoEspecifica() {
+
+            StringBuilder sb = new StringBuilder();
+
+            if (Prioridade == PrioridadeAtendimento.Urgente) {
+
+                sb.AppendLine($"Adicional urgente (30%): R${TaxaAdicional().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+
+            sb.AppendLine($"Taxa de monitoramento: R${TaxaMonitoramento().ToString("F2", CultureInfo.InvariantCulture)}");
+            sb.AppendLine($"Custo total: R${CustoTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+            sb.Append($"Duração estimada: {(int)Duracao().TotalHours} hora(s)");
+
+            return sb.ToString();
         }
     }
 }
